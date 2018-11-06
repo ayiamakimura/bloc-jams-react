a6437b3e2296e007256cae890b29e970057ff639
ayiamakimura <yassinayia@gmail.com>
2018-11-07 00:00:10 +0100

    Assigment 7 completed lokking good

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -9,15 +9,15 @@
          <section id="buttons">
          
            <button id="previous" onClick={this.props.handlePrevClick}>
-             <span className="ion-skip-backward"></span>
+             <span className="icon ion-md-skip-backward"></span>
            </button>
          
            <button id="play-pause" onClick={this.props.handleSongClick}>
-             <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
+             <span className={this.props.isPlaying ? 'ion-md-pause' : 'ion-md-play'}></span>
            </button>
          
            <button id="next" onClick={this.props.handleNextClick}>
-             <span className="ion-skip-forward"></span>
+             <span className="icon ion-md-skip-forward"></span>
            </button>
          
          </section>
@@ -42,9 +42,9 @@
          
          
          <section id="volume-control">
-           <div className="icon ion-volume-low"></div>
+           <div className="icon ion-md-volume-low"></div>
            <input type="range" className="seek-bar" value="80" />
-           <div className="icon ion-volume-high"></div>
+           <div className="icon ion-md-volume-high"></div>
          </section>
        </section>
      );
